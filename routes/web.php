7a1638d7f61b4aa43d23b308b44f9061359eb4d8
RAICHI-Sol <monocloss0965@gmail.com>
2021-04-28 17:13:26 +0900

    routeのグループ化

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Test;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,37 +19,57 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/','App\Http\Controllers\Test@index')->name('home_get');
-
-Route::get('/create',function(){
-    return view('Auth/create_view');
-})->name('create');
-
-Route::get('/make',function(){
-    if(Auth::check()){
-        return view('make');
-    }
-    else{
-        return view('Auth/login');
-    }
-})->name('make_get');
-
-Route::get('/logout',function(){
-    return view('home');
+//ホーム画面でのグループ化
+Route::prefix('')->group(function(){
+    Route::get('/',[Test::class,'index'])->name('home_get');
+    Route::post('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/result', [BoardController::class, 'search'])->name('result');
 });
 
-Route::post('/logout','App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+//ボード作成クラス
+Route::prefix('make')->group(function(){
+    Route::get('/',[Test::class,'make_form'])->name('make_get');
+    Route::post('/',[BoardController::class, 'create'])->name('make');
+});
 
-Route::post('/create/add','App\Http\Controllers\Test@create');
+//プロフィール関連のグループ化
+Route::prefix('profile')->group(function(){
+    Route::get('/{id}',[ProfileController::class,'show'])->name('profile_get');
+});
 
-Route::get('/login','App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+//設定関連のグループ化
+Route::prefix('setting')->group(function(){
+    Route::get('/profile',[ProfileController::class,'setting'])->name('set_prof');
+    Route::put('/profile',[ProfileController::class,'update'])->name('setting');
 
-Route::post('/login','App\Http\Controllers\Auth\LoginController@login');
+    Route::get('/account',function(){
+        return view('account');
+    })->name('set_account');
 
-Route::post('/make',[App\Http\Controllers\BoardController::class, 'create'])->name('make');
+    Route::delete('/account/destroy',[Test::class,'destroy'])->name('destroy');
+});
 
-Route::post('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//ログイン処理でのグループ化
+Route::prefix('login')->group(function(){
+    Route::get('/',[LoginController::class,'showLoginForm'])->name('login');
+    Route::post('/',[LoginController::class,'login']);
+});
 
-Route::get('/board/{id}', [App\Http\Controllers\BoardController::class, 'show']);
+//ログアウト処理でのグループ化
+Route::prefix('logout')->group(function(){
+    Route::get('/',function(){
+        return view('home');
+    });
+    Route::post('/',[LoginController::class,'logout'])->name('logout');
+});
 
-Route::get('/result', [App\Http\Controllers\BoardController::class, 'search'])->name('result');
+//アカウント作成処理でのグループ化
+Route::prefix('create')->group(function(){
+    Route::get('/',function(){
+        return view('Auth/create_view');
+    })->name('create');
+
+    Route::post('/add',[Test::class,'create']);
+});
+
+Route::get('/board/{id}', [BoardController::class, 'show']);
