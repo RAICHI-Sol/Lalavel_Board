e29f28dfe6de5fcc83cd211d9a3ba15c24fcaf22
RAICHI-Sol <monocloss0965@gmail.com>
2021-04-28 17:10:07 +0900

    DB「profile」マイグレーションの追加

---
+++ database/migrations/2021_04_27_141028_create_profile.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('userid');
+            $table->string('sex')->nullable();
+            $table->string('from')->nullable();
+            $table->integer('old')->nullable();
+            $table->string('job')->nullable();
+            $table->string('profile',1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
