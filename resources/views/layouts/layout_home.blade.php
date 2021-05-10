25be0d2bb613972bf91888a7074cda7920bcd213
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-10 13:53:38 +0900

    cssの読み込みをsecure_assetに対応

--- resources/views/layouts/layout_home.blade.php
+++ resources/views/layouts/layout_home.blade.php
@@ -5,7 +5,7 @@
         <meta charset = "UTF-8">
         <meta name = "viewport" >
         <meta http-equiv="X-UA-Compatible" content="ie = edge">
-        <link rel = "stylesheet" href = "/laravel/resources/css/app.css">
+        <link rel = "stylesheet" href = "{{ secure_asset('app.css')}}">
         <link href="https://fonts.googleapis.com/css?family=Cherry+Swash:700" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet">
