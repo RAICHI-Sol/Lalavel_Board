63e51a03b80759bc5208b7c65ad642d46f552923
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-10 09:26:56 +0900

    viewportをスマートフォンのサイズに対応

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset = "UTF-8">
-        <meta name = "viewport" >
+        <meta name = "viewport" content="width=device-width,initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie = edge">
         <link rel = "stylesheet" href = "/laravel/resources/css/app.css">
         <link href="https://fonts.googleapis.com/css?family=Cherry+Swash:700" rel="stylesheet">
