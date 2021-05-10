2ab7900c5722548a412629c78cc900c059bd0f3b
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-10 09:33:13 +0900

    変数をEloquentのリレーション用に変更

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/layout_home')
 
-@section("title",$profile->name.'さんのプロフィール')
+@section("title",$user->name.'さんのプロフィール')
 
 @section('main')
 <div class = "profile">
@@ -9,17 +9,17 @@
         <div class = "icon"></div>
         <div class = "text">
             @php
-                echo '<p><b>名前</b>：'.$profile->name.'</p>';
-                if($profile->sex !=NULL)  echo '<p><b>性別</b>：'.$profile->sex.'</p>';
-                if($profile->from !=NULL) echo '<p><b>出身</b>：'.$profile->from.'</p>';
-                if($profile->old !=NULL)  echo '<p><b>年齢</b>：'.$profile->old.'歳</p>';
-                if($profile->job !=NULL)  echo '<p><b>職種</b>：'.$profile->job.'</p>';
+                echo '<p><b>名前</b>：'.$user->name.'</p>';
+                if($user->profile->sex !=NULL)  echo '<p><b>性別</b>：'.$user->profile->sex.'</p>';
+                if($user->profile->from !=NULL) echo '<p><b>出身</b>：'.$user->profile->from.'</p>';
+                if($user->profile->old !=NULL)  echo '<p><b>年齢</b>：'.$user->profile->old.'歳</p>';
+                if($user->profile->job !=NULL)  echo '<p><b>職種</b>：'.$user->profile->job.'</p>';
             @endphp
         </div>
     </article>
     <article class = "details">
         <h2><i class="fas fa-clipboard-list"></i>自己紹介</h2>
-        <p>{{$profile->profile}}</p>
+        <p>{{$user->profile->profile}}</p>
     </article>
 </div>
 @endsection
