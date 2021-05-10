a39af6a708535d0522e2b45df54ae91a63d94b80
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-10 09:33:59 +0900

    「話したい」ボタンの実装

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -17,10 +17,25 @@
         <p class = "date">
             投稿日：{{$boards->created_at}}
             <span class = "space"></span>
-            <a href = {{route('profile_get',$boards->create_userid)}}>投稿者：{{$boards->name}}</a>
+            <a href = {{route('profile_get',$boards->create_userid)}}>
+                投稿者：{{$boards->user->name}}
+            </a>
         </p>
         <h2>概要説明</h2>
-        <?php echo '<p class = "gide">'.nl2br(htmlspecialchars($boards->comment)).'</p>' ?>
+        <?php echo '<p class = "gide">'.nl2br(htmlspecialchars($boards->comment->comment)).'</p>'?>
+        @if(Auth::check())
+            <a href = {{route('chat',$boards->id)}} class = "contact">
+                @if(Auth::id() != $boards->create_userid)
+                    <i class="fas fa-comments"></i>話してみたい！
+                @else
+                    <i class="fas fa-comments"></i>トークを確認する
+                @endif
+            </a>
+        @else
+            <a href = {{route('chat',$boards->id)}} class = "contact">
+                <i class="fas fa-comments"></i>話してみたい！
+            </a>
+        @endif
     </article>
 </div>
 @endsection
