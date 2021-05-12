f27054b00b1bc3ad37201b80e03699105c898d12
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-12 13:41:25 +0900

    ボードの編集/削除機能の実装

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -11,31 +11,65 @@
 @endsection
 
 @section('main')
+<script src = /laravel/resources/js/make_board.js></script>
 <div class = "boards">
-    <h1>{{$boards->board_name}}</h1>
+    <?php
+        $comment = str_replace("\n","<br>",$boards->comment->comment);
+        $name    = $boards->board_name;
+        $id      = $boards->id;
+        $userid  = $boards->create_userid;
+    ?>
+    <h1>{{$name}}</h1>
     <article class = "comment">
         <p class = "date">
             投稿日：{{$boards->created_at}}
             <span class = "space"></span>
-            <a href = {{route('profile_get',$boards->create_userid)}}>
+            <a href = {{route('profile_get',$userid)}}>
                 投稿者：{{$boards->user->name}}
             </a>
         </p>
-        <h2>概要説明</h2>
-        <?php echo '<p class = "gide">'.nl2br(htmlspecialchars($boards->comment->comment)).'</p>'?>
+        <h2>
+            概要説明
+            @if(Auth::id() == $userid)
+                <label for = "edit{{$id}}">
+                    <i class="fas fa-pen-square blue"></i>
+                    <button id = "edit{{$id}}"
+                    onclick = "click_edit({{$id}},'{{$name}}','{{$comment}}')"></button>
+                </label>
+                <label for = "delete{{$id}}">
+                    <i class="fas fa-minus-square red"></i>
+                    <button id = "delete{{$id}}" onclick = "click_delete({{$id}})"></button>
+                </label>
+            @endif
+        </h2>
+        <p class = "subcomment">{{$boards->comment->comment}}</p>
         @if(Auth::check())
-            <a href = {{route('chat',$boards->id)}} class = "contact">
-                @if(Auth::id() != $boards->create_userid)
+            <a href = {{route('chat',$id)}} class = "contact">
+                @if(Auth::id() != $userid)
                     <i class="fas fa-comments"></i>話してみたい！
                 @else
                     <i class="fas fa-comments"></i>トークを確認する
                 @endif
             </a>
         @else
-            <a href = {{route('chat',$boards->id)}} class = "contact">
+            <a href = {{route('chat',$id)}} class = "contact">
                 <i class="fas fa-comments"></i>話してみたい！
             </a>
         @endif
     </article>
 </div>
+@endsection
+
+@section('fixed')
+    <div class='fixed'>
+        <div class="editbox">
+            <p class ='head'>
+                ボードを編集
+                <i class='fas fa-times' id = 'remove'></i>
+            </p>
+            <input type = 'text' name = 'name' required>
+            <textarea rows = '8' cols='40' name = 'comment' required wrap='hard'></textarea>
+            <input type = 'submit' id = 'submit' value = '編集完了'>
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,5 @@
+@inject('Test', 'App\Http\Controllers\Test')
+
 @extends('layouts/layout_home')
 
 @section("title",'Mypage')
@@ -11,36 +13,78 @@
 @endsection
 
 @section('main')
+<script src = /laravel/resources/js/make_board.js></script>
 <div class = "boards">
     <h1>{{$title}}</h1>
     @if($boards->isEmpty())
         <p>該当するボードがございません。</p>
     @else
         @foreach($boards as $board)
+            <?php
+                $comment = str_replace("\n","<br>",$board->comment->comment);
+                $name    = $board->board_name;
+                $id      = $board->id;
+                $userid  = $board->create_userid;
+            ?>
             <article class = "board">
-                <label for = "board{{$board->id}}">
+                <label for = "board{{$id}}">
                     <p class = "date">
                         投稿日：{{$board->created_at}}
                         <span class = "space"></span>
-                        <a href = {{route('profile_get',$board->create_userid)}}>投稿者：{{$board->user->name}}</a>
+                        <a href = {{route('profile_get',$userid)}}>
+                            投稿者：{{$board->user->name}}
+                        </a>
                     </p>
                     <p>{{$board->board_name}}</p>
-                    <button onclick = "location.href = './board/{{$board->id}}'" id = "board{{$board->id}}"></button>
+                    @if(Auth::id() == $userid)
+                        <label for = "edit{{$id}}">
+                            <i class="fas fa-pen-square blue"></i>
+                            <button id = "edit{{$id}}" onclick = "click_edit({{$id}},'{{$name}}','{{$comment}}')"></button>
+                        </label>
+                        <label for = "delete{{$id}}">
+                            <i class="fas fa-minus-square red"></i>
+                            <button id = "delete{{$id}}" onclick = "click_delete({{$id}})"></button>
+                        </label>
+                    @endif
+                    <button onclick = "location.href = './board/{{$id}}'" id = "board{{$id}}"></button>
                 </label>
             </article>
         @endforeach
         <div class = "button_frame">
             @for($i = 0,$j = 0;$i <= $count;$i++)
                 @if($i % 6 == 0)
-                    @if($title =='Home')
-                        <?php $url = './?page='.$j ?>
-                    @else
-                        <?php $url = './result/?page='.$j.'&search='.$search ?>
-                    @endif
-                    <button type = "button" onclick = "location.href = '{{$url}}'">{{++$j}}</button>
+                    <button type = "button" onclick = "location.href = '{{$Test->get_url($title,$j,$search)}}'">
+                        {{++$j}}
+                    </button>
                 @endif
             @endfor
         </div>
     @endif
 </div>
+@endsection
+
+@section('fixed')
+    <div class='fixed'>
+        <div class="editbox">
+            <p class ='head'>
+                ボードを編集
+                <i class='fas fa-times' id = 'remove'></i>
+            </p>
+            <input type = 'text' name = 'name' required>
+            <textarea rows = '8' cols='40' name = 'comment' required wrap='hard'></textarea>
+            <input type = 'submit' id = 'submit' value = '編集完了'>
+        </div>
+    </div>
+@endsection
+
+
+@section('script')
+    @if(Request::is('make'))
+        <p class='success_comment'>ボードが投稿されました</p>
+        <script>
+            setTimeout(function(){
+                $(".success_comment").remove();
+            }, 3000);
+        </script>
+    @endif
 @endsection
