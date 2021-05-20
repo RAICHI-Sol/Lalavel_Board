1b6da18eecc99f7d2ef18800326a5a4b6caf3cd0
RAICHI-Sol <monocloss0965@gmail.com>
2021-05-20 14:24:33 +0900

    細かな修正

--- resources/views/create_view.blade.php
+++ resources/views/create_view.blade.php
@@ -3,25 +3,25 @@
 @section("title",'新規アカウント作成')
 
 @section('main')
-    <form action = "create/add" class = "create_form" method = "POST" autocomplete="off">
+    <form action = "{{route('createCheck')}}" class = "create_form" method = "POST" autocomplete="off">
         @csrf
         @method('post')
         <h1>アカウント作成</h1>
         <p>Name</p>
-        <input type = "text" name = "name" maxlength ="20"　required
-        placeholder = "Username" class = "@error('name') is-invalid @enderror">
+        <input type = "text" name = "name" placeholder = "Username" value = "{{old('name')}}"
+        class = "@error('name') is-invalid @enderror">
         @error('name')
             <p class = 'error'>{{ $message }}</p>
         @enderror
         <p>UserID</p>
-        <input type = "email" name = "email" maxlength ="30" required
-        placeholder = "UserID" class = "@error('email') is-invalid @enderror">
+        <input type = "email" name = "email" placeholder = "UserID" value = "{{old('email')}}"
+        class = "@error('email') is-invalid @enderror">
         @error('email')
             <p class = 'error'>{{ $message }}</p>
         @enderror
         <p>Password</p>
-        <input type = "password" name = "password" maxlength ="16" minlength="8" required
-        placeholder = "Password" class = "@error('password') is-invalid @enderror">
+        <input type = "password" name = "password" placeholder = "Password" value = "{{old('password')}}"
+        class = "@error('password') is-invalid @enderror">
         @error('password')
             <p class = 'error'>{{ $message }}</p>
         @enderror
